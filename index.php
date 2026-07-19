6a61533a49624e8c4303e2e7b4bb0134641ac78c
noumanhabibaws <nouman.awscloud@gmail.com>
2026-07-19 14:14:38 +0500

    Add index.php for CI/CD pipeline demo

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>CI/CD Pipeline Demo!</h1>";
+echo "<p>Deployed automatically via AWS CodePipeline</p>";
+echo "<p>Author: Nouman Habib</p>";
+echo "<p>Last updated: " . date("Y-m-d H:i:s") . "</p>";
+?>
